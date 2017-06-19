440f8334438e3505cb5fab562c7486017a0dc921
jared edwards <jredwards519@gmail.com>
2017-06-19 09:41:44 -0400

    using gulp to build templates for each state and national

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,26 @@ const rename = require('gulp-rename');
 const csv = require('fast-csv');
 const fs = require('fs');
 
- 
+/*
+
+gulp.task('default', function(){
+  var stream = fs.createReadStream("stock_photo_coolness.csv");
+  
+  var csvStream = csv({headers: true, objectMode: true})
+  .on("data", function(data){
+    console.log(data);
+  })
+  .on("end", function(){
+    console.log("done");
+  });
+  
+  stream.pipe(csvStream);
+})
+*/
+
+// icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
+
+
 gulp.task('processIndex', function() {
   // place code for your default task here
   gulp.src('views/layout.pug')
@@ -18,11 +37,21 @@ gulp.task('processIndex', function() {
 
 gulp.task('processStates', function(){
   var input = fs.createReadStream('./stock_photo_coolness.csv');
-  var csvStream = csv({objectMode: true})
+  var csvStream = csv({headers: true})
               .on('data', function(data){
+                console.log(data)
                 gulp.src('views/layout.pug')
-                          .pipe(pug({data: {data: data}}))
-                          .pipe(rename(`${data[0]}.html`))
+                          .pipe(pug( {
+                            data: {
+                              demographic: data.Demographic, 
+                              veryCool: data['Very cool'],
+                              cool: data['Cool'],
+                              notSoCool: data['Not so cool'],
+                              dontKnow: data['Dont Know / No Opinion']
+                            }
+                          }
+                        ))
+                          .pipe(rename(`${data.Demographic}.html`))
                           .pipe(gulp.dest(`dist/`));
               })
               .on('end', function(){
